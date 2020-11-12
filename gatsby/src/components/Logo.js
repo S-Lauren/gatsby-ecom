187ad44d48bbc494eb0c918eb93269051b8617bc
Sarah Lauren <s.lauren.contact@gmail.com>
2020-11-12 11:38:58 +0100

    import svg on navigation

---
+++ gatsby/src/components/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import KebabIcon from "../assets/icons/kebab_icon.svg";
+import styled  from 'styled-components';
+const IconContainer = styled.div`
+  width: 100px; 
+  height: 100px;
+  overflow: hidden; 
+
+`
+;
+const Logo = () => {
+  return (
+    <IconContainer>
+      <KebabIcon/>
+    </IconContainer>
+  );
+};
+
+export default Logo;
--- gatsby/src/components/nav.js
+++ gatsby/src/components/nav.js
@@ -1,30 +1,69 @@
 import { Link } from 'gatsby';
 import React from 'react'; 
+import styled from 'styled-components';
+import Logo from './Logo';
 
+const NavStyles = styled.nav`
+  background-color: black;
+  width: 100%; 
+  height: 15vh;  
+  align-items: center; 
+  display: flex; 
+  justify-content: center; 
+  
+  .logo {
+    transform: translateY(-10px);
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    display: flex; 
+    justify-content: center; 
+    align-items: center; 
+  }
+  li {
+    display: flex; 
+    margin-left: 15px; 
+    justify-content: center; 
+    align-items: center; 
+    font-family: 'Calibre';
+    &:nth-child(1) {
+      margin-left: 0; 
+    }
+  }
+  a {
+    color: white; 
+    text-decoration: none; 
+    &:hover {
+      color: var(--red); 
+    }
+  }
 
+`
+;
 
 
 const Nav = () => {
   return (
-    <>
-      <ul>
-        <li>
-          <Link to='/news'>Hot now </Link>
-        </li>
-        <li>  
-          <Link to='/menu'> Kebab Menu </Link>
-        </li>
-        <li>
-          <Link to='/logo'>Logo </Link>
-        </li>
-        <li>  
-          <Link to='/cookers'> Cookers </Link>
-        </li>
-        <li>  
-          <Link to='/order'> Order a Kebab </Link>
-        </li>
-      </ul>
-    </>
+      <NavStyles>
+        <ul>
+          <li>
+            <Link to='/news'>Hot now </Link>
+          </li>
+          <li>  
+            <Link to='/menu'> Kebab Menu </Link>
+          </li>
+          <li className="logo" >
+            <Link to='/logo'><Logo/> </Link>
+          </li>
+          <li>  
+            <Link to='/cookers'> Cookers </Link>
+          </li>
+          <li>  
+            <Link to='/order'> Order a Kebab </Link>
+          </li>
+        </ul>
+      </NavStyles>
   )
 
 }
